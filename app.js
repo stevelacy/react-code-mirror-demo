0ecedde8e945769760bf7e6fa360ca777f750ca5
Steve Lacy <me@slacy.me>
2015-04-29 11:29:11 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var React = require('react');
+var CodeMirror = React.createFactory(require('react-code-mirror'));
+require('codemirror/mode/javascript/javascript');
+require('codemirror/addon/selection/active-line');
+require('codemirror/addon/edit/matchtags');
+require('codemirror/keymap/sublime');
+
+var DOM = React.DOM;
+var src = 'var myFunc = function() {\n' +
+          '\n' +
+          '}\n';
+
+var app = React.createClass({
+  getInitialState: function() {
+    return {
+      src: src
+    };
+  },
+  render: function() {
+    return DOM.div({
+      className: 'wrapper',
+    },
+      CodeMirror({
+        className: 'editor',
+        mode: 'javascript',
+        theme: 'monokai',
+        keyMap: 'sublime',
+        autoCloseBrackets: true,
+        defaultValue: this.state.src,
+        lineNumbers: true,
+        styleActiveLine: true,
+        lineWrapping: true,
+        styleSelectedText: true,
+        matchBrackets: true,
+        showCursorWhenSelecting: true,
+        viewportMargin: Infinity
+      })
+    );
+  }
+});
+
+React.render(React.createFactory(app)(), document.body);
